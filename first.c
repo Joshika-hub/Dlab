75c183c29f0c7b0e0e6d8352f5b2ff963a489871
Joshika <joshikatak@gmail.com>
2025-09-03 12:20:33 +0530

    hello

---
+++ first.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int a,b,c;
+float sum,sub,mul,div;
+printf("Enter the value");
+scanf("&d",a);
+printf("Enter the value");
+scanf("&d",b);
+printf("Enter the value");
+scanf("&d",c);
+printf(sum=a+b+c);
+printf(sub=a-b-c);
+printf(sum=a*b*c);
+printf(sum=a/c);
